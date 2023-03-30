5d6cf35a2d7109c3094c655efa9529822344ed31
Julius Nguyen <92987090+julius-nguyen@users.noreply.github.com>
2023-03-30 20:15:07 +0200

    Update happiness_score.hs

--- happiness_score.hs
+++ happiness_score.hs
@@ -11,15 +11,8 @@ setB = Set.fromList [5,7]
 numbers :: [Int]
 numbers = [1,5,3]
 
-happiness :: [Int] -> Set.Set Int -> Set.Set Int -> Int 
-happiness [] _ _ = 0
-happiness (n:ns) set1 set2
-  | Set.member n set1 = 1 + happiness ns set1 set2 
-  | Set.member n set2 = -1 + happiness ns set1 set2 
-  | otherwise = happiness ns set1 set2
-
-happinessF :: [Int] -> Set.Set Int -> Set.Set Int -> Int 
-happinessF (x:xs) set1 set2 = foldr ((+) . cond) 0 (x:xs)
+happinessF :: Set.Set Int -> Set.Set Int -> [Int] -> Int 
+happinessF set1 set2 = foldr ((+) . cond) 0 (x:xs)
   where cond x
             | Set.member x set1 = 1
             | Set.member x set2 = -1
