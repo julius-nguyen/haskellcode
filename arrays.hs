327a673e9c4d7c2d6569555abcf6b6c56f1025e1
Julius Nguyen <92987090+julius-nguyen@users.noreply.github.com>
2023-04-21 11:05:41 +0200

    Create arrays.hs

---
+++ arrays.hs
@@ -0,0 +1,16 @@
+import Control.Monad
+
+flatten :: [[a]] -> [a]
+flatten [] = []
+flatten (xs:xxs) = xs ++ flatten xxs
+
+-- calculates the sum of the values of multiple arrays
+-- first input specifies the number N of arrays
+-- on separate lines we can then enter the array values separated each by white space
+main :: IO ()
+main =
+    do 
+        n <- readLn
+        input <- replicateM n getLine
+        putStrLn (map read (flatten (map words input)))
+        putStrLn (show (sum (intlist (map words strpoints))))  
